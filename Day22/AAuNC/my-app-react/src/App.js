9729690c8ec975ae88e9d4e992242c936eb7a5b3
ngoChungAAu <audavn@gmail.com>
2021-09-08 16:17:50 +0700

    day22 react

--- Day22/AAuNC/my-app-react/src/App.js
+++ Day22/AAuNC/my-app-react/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header.js';
+import Banner from './components/Banner/Banner.js';
+import Text from './components/Text/Text.js';
+import Form from './components/Form/Form.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div id="l-login">
+        <Header></Header>
+        <div id="l-main">
+          <Banner></Banner>
+          <Text></Text>
+          <Form></Form>
+        </div>
+    </div>   
   );
 }
 
